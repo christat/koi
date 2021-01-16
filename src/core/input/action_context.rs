b997d9fe013d18e48261671aeba34e9f38346dfb
Chris Statkiewicz <christopherstat@protonmail.com>
2021-01-16 15:15:26 +0200

    add missing binding enums

--- src/core/input/action_context.rs
+++ src/core/input/action_context.rs
@@ -256,20 +256,20 @@ macro_rules! define_contextual_action_bindings {
 
 define_contextual_action_bindings!(
     MainMenu {
-        { Up,           Button::DPadUp,         None,   Key::Up,        None        }
-        { Down,         Button::DPadDown,       None,   Key::Down,      None        }
-        { Left,         Button::DPadLeft,       None,   Key::Left,      None        }
-        { Right,        Button::DPadRight,      None,   Key::Right,     None        }
+        { Up,           Button::DPadUp,         None,   Key::Up,                None        }
+        { Down,         Button::DPadDown,       None,   Key::Down,              None        }
+        { Left,         Button::DPadLeft,       None,   Key::Left,              None        }
+        { Right,        Button::DPadRight,      None,   Key::Right,             None        }
     }
     InGame {
-        { Forward,      Stick::LSUp,            None,   Key::W,         None        }
-        { Backward,     Stick::LSDown,          None,   Key::S,         None        }
-        { Left,         Stick::LSLeft,          None,   Key::A,         None        }
-        { Right,        Stick::LSRight,         None,   Key::D,         None        }
-        { LookUp,       Stick::RSUp,            None,   None,           None        }
-        { LookDown,     Stick::RSDown,          None,   None,           None        }
-        { LookLeft,     Stick::RSLeft,          None,   None,           None        }
-        { LookRight,    Stick::RSRight,         None,   None,           None        }
-        { Sprint,       Button::LeftBumper,     None,   Key::LShift,    Key::RShift }
+        { Forward,      Stick::LSUp,            None,   Key::W,                 None        }
+        { Backward,     Stick::LSDown,          None,   Key::S,                 None        }
+        { Left,         Stick::LSLeft,          None,   Key::A,                 None        }
+        { Right,        Stick::LSRight,         None,   Key::D,                 None        }
+        { LookUp,       Stick::RSUp,            None,   MouseMotion::YUp,       None        }
+        { LookDown,     Stick::RSDown,          None,   MouseMotion::YDown,     None        }
+        { LookLeft,     Stick::RSLeft,          None,   MouseMotion::XLeft,     None        }
+        { LookRight,    Stick::RSRight,         None,   MouseMotion::XRight,    None        }
+        { Sprint,       Button::LeftBumper,     None,   Key::LShift,            Key::RShift }
     }
 );
